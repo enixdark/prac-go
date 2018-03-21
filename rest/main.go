cf6d6e5520d9790840fcea59109cb667a1d3fc0d
enixdark <cqshinn92@gmail.com>
2018-03-22 04:09:19 +0700

    update service

--- rest/main.go
+++ rest/main.go
@@ -5,11 +5,19 @@ import (
 	"fmt"
 	"log"
 
-	// "github.com/enixdark/prac-go/rest/src/rest"
+	"github.com/enixdark/prac-go/rest/src/rest"
 	"github.com/enixdark/prac-go/rest/src/lib/configuration"
 	"github.com/enixdark/prac-go/rest/src/lib/persistence/dblayer"
 )
 
 func main() {
-	println("hello world");
+	confPath := flag.String("conf", `./configuration/config.json`, "flag to set the path to the configuration json file")
+	flag.Parse()
+
+	config, _ := configuration.ExtractConfiguration(*confPath)
+
+	fmt.Println("Connecting to database")
+	dbhandler, _ := dblayer.NewPersistenceLayer(config.Databasetype, config.DBConnection)
+
+	log.Fatal(rest.ServeAPI(config.RestfulEndpoint, dbhandler))
 }
